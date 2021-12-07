a6bf62b5d02b41bd676008ff6d4c5f5e64bb9692
altescy <altescy@fastmail.com>
2021-12-07 12:59:21 +0900

    remove importlib-metadata

--- queuery_client/__init__.py
+++ queuery_client/__init__.py
@@ -1,5 +1,5 @@
-from importlib_metadata import version
+from importlib.metadata import version
 
 from queuery_client.queuery_client import QueueryClient  # NOQA: F401
 
-__version__ = version("queuery_client")  # type: ignore[no-untyped-call]
+__version__ = version("queuery_client")
